37d5520c93cc6cf63f39525999c68f9740cde5ea
Sixto Becerra <wow1sixto@gmail.com>
2022-08-10 20:35:00 -0500

    Se saluda a Juan Pablo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import UseState from "./components/useState";
 function App() {
 	return (
 		<div>
-			<h1 className="center">Hello World!!!</h1>
+			<h1 className="center">Hello JP!!!</h1>
 			<UseState />
 		</div>
 	);
